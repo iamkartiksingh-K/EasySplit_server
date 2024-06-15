51163a0902eae353218272f4dad20558e8c4a925
iamkartiksingh-k <kartik01022001@gmail.com>
2024-06-15 20:04:22 +0530

    changed origin in cors

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ import groupRoutes from "./src/routes/groupRoutes.js";
 import expenseRoutes from "./src/routes/expenseRoute.js";
 import cookieParser from "cookie-parser";
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+	cors({ origin: "https://easy-split-client.vercel.app/", credentials: true })
+);
 app.use(express.json());
 app.use(cookieParser());
 
