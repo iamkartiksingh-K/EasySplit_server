928b872c4a3f76ea9afba5e1db58cab5bf197c0c
Kartik Singh <kartiksinghk2001@gmail.com>
2024-07-25 15:05:00 +0530

    bug fixed

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ import expenseRoutes from "./src/routes/expenseRoute.js";
 import cookieParser from "cookie-parser";
 const app = express();
 app.use(
-	cors({ origin: "https://easy-split-client.vercel.app", credentials: true })
+  cors({ origin: "https://easy-split-client.vercel.app", credentials: true }),
 );
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -20,13 +21,13 @@ app.use("/api/groups", groupRoutes);
 app.use("/api/expenses", expenseRoutes);
 
 mongoose
-	.connect(process.env.DBURI)
-	.then(() => {
-		console.log("Database connected");
-		app.listen(5000, () => {
-			console.log("server is live : http://localhost:5000");
-		});
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+  .connect(process.env.DBURI)
+  .then(() => {
+    console.log("Database connected");
+    app.listen(5000, () => {
+      console.log("server is live : http://localhost:5000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
