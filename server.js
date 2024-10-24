b970dc7f463052a9b9cfa644533e1aef5de2d20b
Kartik Singh <kartiksinghk2001@gmail.com>
2024-10-24 10:32:51 +0530

    added health route

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ import authRouter from "./src/routes/auth.js";
 import userRoutes from "./src/routes/userRoutes.js";
 import groupRoutes from "./src/routes/groupRoutes.js";
 import expenseRoutes from "./src/routes/expenseRoute.js";
+import healthRoute from "./src/routes/health.js"
 import cookieParser from "cookie-parser";
 const app = express();
-app.use(
-  cors({ origin: "https://easy-split-client.vercel.app", credentials: true }),
-);
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(
+//   cors({ origin: "https://easy-split-client.vercel.app", credentials: true }),
+// );
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/health", healthRoute);
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRoutes);
 app.use("/api/groups", groupRoutes);
